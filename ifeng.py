6915eee0ff9d4efeed0b449d9058c51c4bccd84e
dengpeng <dengpeng@outlook.com>
2015-07-02 17:21:17 +0800

    finish ifeng

--- ifeng.py
+++ ifeng.py
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*
-from datetime import datetime
+from datetime import datetime, time
 import logging
 import random
 import requests
@@ -13,7 +13,7 @@ logger = Logger(logname='ifeng.log', logger=__name__).get_logger()
 
 
 class FengWorker(Worker):
-    def __init__(self, startdate=date(2015, 1, 1), enddate=date(2015, 1, 2)):
+    def __init__(self, startdate=date(2009, 7, 1), enddate=date(2015, 6, 5)):
         super(FengWorker, self).__init__()
         self.beginDate = startdate
         self.endDate = enddate
@@ -28,28 +28,31 @@ class FengWorker(Worker):
         day = self.beginDate
         while day <= self.endDate:
             date_str = day.strftime("%Y%m%d")
-            if date_str not in self.historyDict:
+            if date_str not in self.history:
                 self.crawl_by_day(date_str)
             day += Worker.dayDelta
+        self.reget_errorlist()
 
     def get_records(self):
         self.history = Article.objects.distinct('post_date')
 
     def crawl_by_day(self, date_str):
         try:
-            for catg in [11528, 11574, 11502]:
+            catg = {'11528': '大陆', '11574': '国际', '11502': '即时'}
+            for ct in catg.keys():
                 page = 1
                 # 当日首页
-                r = requests.get(self.__listUrl.format(catg, date_str, page))
+                r = requests.get(self.__listUrl.format(ct, date_str, page))
                 if r.status_code == 200:
                     d = pq(r.text)
-                    self.parse_articles_list(d('.newsList'))
+                    self.parse_articles_list(d('.newsList'), date_str, catg[ct])
                     # 循环分页
                     while len(d('.newsList ul li')) == 60:
                         page += 1
-                        r = requests.get(self.__listUrl.format(catg, date_str, page))
+                        r = requests.get(self.__listUrl.format(ct, date_str, page))
                         d = pq(r.text)
-                        self.parse_articles_list(d('.newsList'), date_str)
+                        self.parse_articles_list(d('.newsList'), date_str, catg[ct])
+                        page += 10
             self.save_temp_dict()
         except requests.exceptions.RequestException, e:
             logging.exception(e)
@@ -59,14 +62,16 @@ class FengWorker(Worker):
         finally:
             self.newsDict.clear()
 
-    def parse_articles_list(self, articles, post_date):
+    def parse_articles_list(self, articles, post_date, category):
         d = pq(articles)
         for i in range(0, d('ul li').length):
             li = d('ul li').eq(i)
             title = li('li a').text()
             link = li('li a').attr('href')
-
-            item = {'title': title, 'link': link, 'post_date': post_date}
+            timestr = li('h4').text()
+            post_time = timestr.split(' ')[1]
+            item = {'title': title, 'link': link, 'post_date': post_date, 'category': category,
+                    'post_time': post_time, 'valid': True, 'error_count': 0}
             self.newsDict[link] = item
             # 重试
             retry = 1
@@ -75,29 +80,50 @@ class FengWorker(Worker):
             except StandardError:
                 if retry > 0:
                     retry -= 1
+                    time.sleep(1)
                     self.get_detail(link)
+                else:
+                    self.newsDict[link]['valid'] = False
 
     def get_detail(self, url):
         print(url)
         r = requests.get(url)
         r.encoding = 'utf-8'
         d = pq(r.text)
+        if d('div.tips404'):
+            self.newsDict[url]['valid'] = False
+            print('404 page not found')
+            return
+        d('.ifengLogo').remove()
+        d('script').remove()
         source = ''
         source_link = ''
-        self.newsDict[url]['summary'] = get_content_between(r.text, 'og:description" content="', '"/>')
+        if d('[itemprop=description]'):
+            self.newsDict[url]['summary'] = d('[itemprop=description]').attr('content')
+        elif d('[name=description]'):
+            self.newsDict[url]['summary'] = d('[name=description]').attr('content')
+        self.newsDict[url]['content'] = d('#main_content p').text()
+        if len(self.newsDict[url]['content']) == 0:
+            self.newsDict[url]['content'] = d('#artical_real p').text()
+        self.newsDict[url]['image_links'] = [i.attr('src') for i in d.items('#artical_real p img')]
+        self.newsDict[url]['video_links'] = ['http://v.ifeng.com/include/exterior.swf?' + i.attr('flashvars') for i in
+                                             d.items('#artical_real object embed')]
         if d('#artical_real'):
             if d('#artical_sth .ss03 a'):
                 source = d('#artical_sth .ss03 a').text()
                 source_link = d('#artical_sth .ss03 a').attr.href
             self.newsDict[url]['source'] = source
             self.newsDict[url]['source_link'] = source_link
-            # 可能会包含摘要，能移除的先移除
-            if d('#main_content'):
-                self.newsDict[url]['content'] = d('#main_content p').text()
-            else:
-                self.newsDict[url]['content'] = d('#artical_real p').text()
-            self.newsDict[url]['image_links'] = [i.attr('src') for i in d.items('#artical_real p img')]
-            self.newsDict[url]['video_links'] = [i.attr('flashvars') for i in d.items('#artical_real OBJECT embed')]
+        elif d('#source_place'):
+            self.newsDict[url]['source'] = d('#source_place').text()
+            if d('#source_place a'):
+                source_link = d('#source_place a').attr.href
+            self.newsDict[url]['source_link'] = source_link
+        elif d('#artical_sth > p > span:nth-child(3)'):
+            self.newsDict[url]['source'] = d('#artical_sth > p > span:nth-child(3)').text()
+            if d('#artical_sth > p > span:nth-child(3) a'):
+                source_link = d('#artical_sth > p > span:nth-child(3) a').attr.href
+            self.newsDict[url]['source_link'] = source_link
         nums = self.get_comment_num(url)
         self.newsDict[url]['comment_num'] = nums[0]
         self.newsDict[url]['reply_num'] = nums[1]
@@ -117,6 +143,3 @@ class FengWorker(Worker):
                 return nums[0], '0'
         else:
             return '0', '0'
-
-    __commentNumUrlNew = 'http://coral.qq.com/article/{0}/commentnum'
-
